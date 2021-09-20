4acbdc05efc76bef4e2a2ef1e7143c2e6cb750a0
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-09-20 10:54:56 -0400

    update fecth info

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,13 @@
-var getUserRepos = function(){
-    fetch("https://api.github.com/users/octocat/repos");
-}
-getUserRepos();
+var getUserRepos = function(user){
+    // format the github api url
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+    
+    //make a reuqest to the url
+    fetch(apiURL).then(function(response){
+        console.log(response);
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+};
+getUserRepos("microsoft");
